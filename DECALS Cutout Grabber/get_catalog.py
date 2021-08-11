765455a3babfcfb6ee3b5b1a2216adac34fc5464
Benjamin Levine <Ben@MacBook-Protato.local>
2021-08-11 13:54:11 -0500

    Added multithreading flag

--- DECALS Cutout Grabber/get_catalog.py
+++ DECALS Cutout Grabber/get_catalog.py
@@ -13,8 +13,26 @@ def argument_parser():
     result.add_argument('-c', dest='cutouts', type=str, default="catalogs.txt") 
     result.add_argument('-d', dest='directory', type=str, default="catalogs.nosync") 
     result.add_argument('-l', dest='layer', type=str, default="ls-dr9")
+    result.add_argument('-m', dest='multithread', type=bool, default=False)
 
     return result
+
+def download_catalog(cutout):
+    ra, dec, size = cutout
+    ra_min = ra-size; ra_max = ra+size
+    dec_min = dec-size; dec_max = dec+size
+
+    outname = '/'+str(ra)+'_'+str(dec)+'.fits'
+    if not os.path.isfile(outputs+outname):
+        try:
+            urlib.urlretrieve(url.format(args.layer, ra_min, ra_max, dec_min, dec_max),outputs+outname)
+        except Exception as e:
+            print('Catalog at [', ra, dec, '] timed out. :(')
+            return None
+
+        print('Catalog at [', ra, dec, '] has been downloaded.')
+    else:
+        print('Catalog at [', ra, dec, '] was already downloaded.')
 
 
 if __name__ == '__main__': 
@@ -35,23 +53,16 @@ if __name__ == '__main__':
 
     url = 'https://www.legacysurvey.org/viewer/{}/cat.fits?ralo={}&rahi={}&declo={}&dechi={}'
 
+    socket.setdefaulttimeout(30)
+
     # loop through each cutout and download the data
-    for cutout in cutout_list:
-        ra, dec, size = cutout
-        ra_min = ra-size; ra_max = ra+size
-        dec_min = dec-size; dec_max = dec+size
-
-        outname = '/'+str(ra)+'_'+str(dec)+'.fits'
-
-        socket.setdefaulttimeout(15)
-
-        try:
-            urlib.urlretrieve(url.format(args.layer, ra_min, ra_max, dec_min, dec_max),outputs+outname)
-        except Exception as e:
-            print('Catalog at [', ra, dec, '] timed out. :(')
-            continue
-
-        print('Catalog at [', ra, dec, '] has been downloaded.')
+    if not args.multithread:
+        for cutout in cutout_list:
+                    download_catalog(cutout)
+    else:
+        from multiprocessing import Pool
+        with Pool() as p:
+            p.map(download_catalog, cutout_list)
     
     print('---------')
     print('Done.')
